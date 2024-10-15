47e8b5ab9888e6d06a372916632c3abeb3c3efc3
Yash Yash <yash@easecommerce.in>
2024-10-16 02:48:49 +0530

    changed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/geo-divorce-laws',
+  
   plugins: [react()],
+  base: '/geo-divorce-laws/',
 })
